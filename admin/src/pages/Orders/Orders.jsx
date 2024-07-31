d65181dd11a4ee799470b0a87087cd184612a5bf
mighty808 <effahaboagye888@gmail.com>
2024-07-31 21:33:02 +0000

    edited some pages and commiting

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -60,9 +60,9 @@ const Order = () => {
               <p className='order-item-phone'>{order.address.phone}</p>
             </div>
             <p>Items : {order.items.length}</p>
-            <p>{currency}{order.amount}</p>
+            <p>GH₵ {order.amount}</p>
             <select onChange={(e) => statusHandler(e, order._id)} value={order.status} name="" id="">
-              <option value="Food Processing">Food Processing</option>
+              <option value="Products Processing">Products Processing</option>
               <option value="Out for delivery">Out for delivery</option>
               <option value="Delivered">Delivered</option>
             </select>
